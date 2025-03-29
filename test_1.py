79137cf82adf9870cd06b0cfdfc976ec1f4cfb16
Danielbar9898 <danielbaron.1808@gmail.com>
2025-03-29 22:58:34 +0300

    added funtion to get view by id and workbook by id

--- test_1.py
+++ test_1.py
@@ -32,5 +32,38 @@ def auth():
 
     return token, site_id
 
+def get_workbook_id(token, site_id, workbook_name):
+    url = f"https://dub01.online.tableau.com/api/3.4/sites/{site_id}/workbooks"
+    headers = {
+        "X-Tableau-Auth": token
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get workbooks: {response.status_code} - {response.text}")
+
+    root = ET.fromstring(response.text)
+    namespace = {'t': 'http://tableau.com/api'}
+    for workbook in root.findall(".//t:workbook", namespace):
+        name = workbook.attrib.get("name")
+        if name == workbook_name:
+            return workbook.attrib["id"]
+
+    raise Exception(f'Workbook "{workbook_name}" not found')
+
+def get_view_id(token, site_id, workbook_id, view_name=None):
+    url = f"https://dub01.online.tableau.com/api/3.4/sites/{site_id}/workbooks/{workbook_id}/views"
+    headers = { "X-Tableau-Auth": token }
+
+    namespace = {'t': 'http://tableau.com/api'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get views: {response.status_code} - {response.text}")
+
+    root = ET.fromstring(response.text)
+    for view in root.findall(".//t:view", namespace):
+        if view_name is None or view.attrib.get("name") == view_name:
+            return view.attrib["id"]
+
+    raise Exception(f'View "{view_name or "[any]"}" not found')
 
 
